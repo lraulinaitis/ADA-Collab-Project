09d5b64e9f460f9b608cf9a4be0409acea1d5e6b
Livia <lraulinaitis@gmail.com>
2024-04-25 12:35:09 -0500

    commit3

--- Package/R/hello.R
+++ Package/R/hello.R
@@ -18,69 +18,73 @@
 #' @param d species interaction matrix data set
 #' @param alternative network metric to produce, defaults to "all".
 #' @param plot produce network visualization, defaults to TRUE
-#' @keywords power
+#' @keywords networks
 #' @export
 #' @examples
 #' network(d = NULL, alternative = "all", plot = TRUE)
 
 
-network <- function(d, plot = TRUE, ) {
+network <- function(d, alternative = "all", plot = TRUE) {
 
-}
+  # 1. read in matrix
+  matrix <- read.csv("hummingbirdmatrix.csv", header = T) # edit file name
 
-load_matrix <- function(x) {
-  hummingbird.comm <- read.csv("hummingbirdmatrix.csv", header = T)
-}
+  # 2. produce visualization
 
-create_matrix <- function(x) {
-  uw.comm <- read.csv("medgarden.csv", header = T) %>%
-    group_by(plant, pollinator) %>%
-    summarise(count = sum(count)) %>%
-    pivot_wider(names_from = "pollinator",
-                values_from = "count",
-                values_fill = 0) %>%
-    column_to_rownames("plant")
-}
+  plotweb(sortweb(hummingbird.comm),
+          method = "normal",
+          labsize = 1,
+          text.rot = 90,
+          y.lim = c(-3, 4),
+          col.interaction = "black",
+          col.high = "maroon",
+          col.low = "darkgreen")
 
-create_network <- function(d, ){}
+  visweb(as.matrix(hummingbird.comm),
+         preynames = F,
+         labsize = 2)
 
-network_metrics <- function(d) {
+  # 3. produce network metrics
+
+  robust <- bipartite::networklevel(hummingbird.comm, index = "robustness")
+
+  cat("Robustness (high level): ", robust[1,2], "Robustness (low level): ", robust[2,2])
 
   if (metric == "C") {
     conn <- bipartite::networklevel(d, index = "connectance")
     cat("Connectance: percent of the possible interactions (or node-to-node connections)
-    that actually occur in the network."\n"Calculated by counting the total number
-    of cells with non-zero values in the matrix and dividing by the total number of
-    matrix cells.")
+  that actually occur in the network."\n"Calculated by counting the total number
+  of cells with non-zero values in the matrix and dividing by the total number of
+  matrix cells.")
 
     cat("The connectance value for this network is ", conn)
   }
 
   if (metric == "C") {
-  networklevel(hummingbird.comm, index = "weighted NODF")
-  cat("`weightedNODF`: nestedness, or the extent to which more specialized species
-  interact with a subset of more generalized species. A high nestedness value
-  implies that there is a strong non-random structure to this network, while a
-  low nestedness value implies that interactions are essentially randomly
-  distributed. Because this is a weighted value, it accounts for the interaction
-  strength (how many times does that interaction occur).")
+    networklevel(hummingbird.comm, index = "weighted NODF")
+    cat("`weightedNODF`: nestedness, or the extent to which more specialized species
+interact with a subset of more generalized species. A high nestedness value
+implies that there is a strong non-random structure to this network, while a
+low nestedness value implies that interactions are essentially randomly
+distributed. Because this is a weighted value, it accounts for the interaction
+strength (how many times does that interaction occur).")
 
   }
 
   if (metric == "C") {
     cat("`robustness`: robustness of the network to removal of species, calculated in
-    terms of removal of higher trophic level (HL) and lower trophic level (LL)
-    species. This is calculated as the area under the curve, which means that a
-    higher robustness value means a more gradual negative slope (removal of one
-    species does not generally cause too many secondary extinctions), and a low
-    robustness value means a steep negative slope (removal of one species generally
-    causes many secondary extinctions).")
+  terms of removal of higher trophic level (HL) and lower trophic level (LL)
+  species. This is calculated as the area under the curve, which means that a
+  higher robustness value means a more gradual negative slope (removal of one
+  species does not generally cause too many secondary extinctions), and a low
+  robustness value means a steep negative slope (removal of one species generally
+  causes many secondary extinctions).")
     networklevel(hummingbird.comm, index = "robustness")
 
   }
 
   if (metric == "C") {
-  cat("Specialization (H'): robustness of the network to removal of species, calculated in
+      cat("Specialization (H'): robustness of the network to removal of species, calculated in
     terms of removal of higher trophic level (HL) and lower trophic level (LL)
     species. This is calculated as the area under the curve, which means that a
     higher robustness value means a more gradual negative slope (removal of one
@@ -89,6 +93,9 @@ network_metrics <- function(d) {
     causes many secondary extinctions).")
 
 
+    }
 
-}
+
+
+
 
